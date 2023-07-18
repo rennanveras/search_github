8bb0a5483e150c0b84a9c8934e7e168b3727a717
rennan veras <rennanhenriqueneves@gmail.com>
2023-07-18 15:06:02 -0300

    adicionando um catch para capturar o erro quando não encrontrar o usuario

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -5,16 +5,34 @@ import '../cores.css';
 const ReposList = ({ github, corCSS }) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
+    const [erroAPI, setErroAPI] = useState(false);
 
     useEffect(() => {
         setEstaCarregando(true);
+        setErroAPI(false); // Resetando o estado de erro antes de uma nova requisição
+
         fetch(`https://api.github.com/users/${github}/repos`)
-            .then(res => res.json())
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else if (res.status === 404) {
+                    throw new Error("Usuário não encontrado");
+                }
+                throw new Error(res.statusText);
+            })
             .then(resJson => {
                 setEstaCarregando(false);
                 setRepos(resJson);
             })
+            .catch(e => {
+                setEstaCarregando(false);
+                setErroAPI(true);
+            });
     }, [github]);
+
+    if (erroAPI) {
+        return <h1>Ocorreu um erro ao carregar os repositórios.</h1>;
+    }
 
     return (
         <div className="container">
